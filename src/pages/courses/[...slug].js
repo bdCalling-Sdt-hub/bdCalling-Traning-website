3746946a7836273023d47112f02d36b2ebb4fb1e
siffahim <siffahim25@gmail.com>
2024-02-08 14:47:42 +0600

    feat: :sparkles: responsive done

--- src/pages/courses/[...slug].js
+++ src/pages/courses/[...slug].js
@@ -61,7 +61,7 @@ const CourseDetail = () => {
     <div className="container my-10">
       <MetaTag title="Courses Details" />
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-7">
-        <div className="col-span-2">
+        <div className="lg:col-span-2">
           <CourseDetailDescription data={courseDetail} />
         </div>
         <div className="w-full">
